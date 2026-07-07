298bcd5a14fa3760995bbc440d0f7de8bd5acf36
Gabriel SS <gabrielsantosgithubac@gmail.com>
2026-07-06 21:18:49 -0300

    Project Skeleton

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+
+console.log(chalk.green("Hello, mortal Claude!"));
+console.log(chalk.blue("This is a TypeScript project using NodeNext module system and targeting ES2022."));
